12ec9bbe70f0c739adb9212c449ae21863244d16
Ins1eme <insieme1337@gmail.com>
2018-02-22 22:33:56 +0200

    header

--- pomoyka/AvtoDev/dev/static/js/main.js
+++ pomoyka/AvtoDev/dev/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     svg4everybody({});
+
+
+    // (function dropDownMenu(){
+    	var itemDropDown = $(".main-nav__item--repair");
+
+    	itemDropDown.hover(function(){
+    		$(this).find(".repair-nav").toggleClass('is-active');
+    	});
 });
