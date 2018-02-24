62a8d9336056fa86a70b4cfddd376429b8b5d0ca
Ins1eme <insieme1337@gmail.com>
2018-02-24 12:19:30 +0200

    fixed tabs

--- pomoyka/AvtoDev/dev/static/js/main.js
+++ pomoyka/AvtoDev/dev/static/js/main.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
     svg4everybody({});
 
-	var itemDropDown = $(".main-nav__item--repair");
+    (function dropDownMenu($){
+		let itemDropDown = $(".main-nav__item--repair");
 
-	itemDropDown.hover(function(){
-		$(this).find(".repair-nav").toggleClass('is-active');
-	});
+		itemDropDown.hover(function(){
+			$(this).find(".repair-nav").toggleClass('is-active');
+		});
+    	
+    })(jQuery);
 
-	$(".advice-form__inpt--tel").mask('(000) 000-00-00');
+    (function maskInput($){
+
+		$(".advice-form__inpt--tel").mask('(000) 000-00-00');
+
+    })(jQuery);
+
+    (function tabCategories($){
+    	$(".categories-nav__item").on("click", function(){
+    		let tabs = $(".tab-item");
+    		$(this).addClass('is-active').siblings().removeClass('is-active')
+    		.closest('.container').find(".tab-item").removeClass('is-active').eq($(this).index()).addClass('is-active');
+
+    	});
+    })(jQuery);
+
 });
