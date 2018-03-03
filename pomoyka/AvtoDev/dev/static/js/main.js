88f1dc044a98d0d452308be44e68915d82ee8118
Ins1eme <insieme1337@gmail.com>
2018-03-03 11:57:37 +0200

    fixed menu

--- pomoyka/AvtoDev/dev/static/js/main.js
+++ pomoyka/AvtoDev/dev/static/js/main.js
@@ -17,11 +17,12 @@ $(document).ready(function () {
             $(this).addClass('search-active');
             $(".main-nav-wrapper").addClass('search-active');
             $(".cancel-wrapper").find(".icon-cancel").addClass('search-active');
+            $(".logo").addClass('search-active');
         });
 
        $(".search__input").focus(function(){
             $search.find(".icon-search").addClass('search-active');
-            $(".logo").addClass('search-active')
+            $(".logo").addClass('search-active');
         }).blur(function(){
             $search.find(".icon-search").removeClass('search-active');
         })
@@ -33,7 +34,7 @@ $(document).ready(function () {
                 $search.removeClass('search-active');
                 $(".main-nav-wrapper").removeClass('search-active').addClass('is-animated');
                 $(".cancel-wrapper").find(".icon-cancel").removeClass('search-active');
-                 $(".logo").removeClass('search-active')
+                $(".logo").removeClass('search-active')
             }
         })
 
